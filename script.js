8790224a4ccfd2cb50433e2e4558a63887c77d1f
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-04-11 19:33:48 +0300

    Add variables for day of the month and day of the week.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function getDate() {
+function getBirthday() {
     let dateInput = document.getElementById("date").value;
     let date = new Date(dateInput);
 
     if (!!date.valueOf()) {
         let year = date.getFullYear().toString();
+        let month = date.getMonth()+1;
+        let dayOfMonth = date.getDate();
+        let dayOfWeek = date.getDay();
         let CC = parseInt(year.slice(0,2));
-        alert(CC);
+        let YY = parseInt(year.slice(2,4));
+        alert(dayOfWeek);
     }
     else{
         alert("Please select your birthday.")
